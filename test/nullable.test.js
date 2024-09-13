d9b192e11246bdb6f3f1896452f9897fa75faadf
Andrew Plummer <plummer.andrew@gmail.com>
2024-09-13 12:36:54 +0900

    more nullable tests

--- test/nullable.test.js
+++ test/nullable.test.js
@@ -1,5 +1,5 @@
 import yd from '../src';
-import { assertPass } from './utils';
+import { assertPass, assertFail } from './utils';
 
 describe('nullable', () => {
   it('should allow a string to be nullable', async () => {
@@ -11,6 +11,27 @@ describe('nullable', () => {
   it('should allow an object to be nullable', async () => {
     const schema = yd.object().nullable();
     await assertPass(schema, {});
+    await assertPass(schema, null);
+  });
+
+  it('should allow a nullable string', async () => {
+    const schema = yd.string().nullable();
+    await assertFail(schema, '', ['String may not be empty.']);
+    await assertPass(schema, null);
+  });
+
+  it('should allow an empty nullable string', async () => {
+    const schema = yd.string().allowEmpty().nullable();
+    await assertPass(schema, '');
+    await assertPass(schema, null);
+  });
+
+  it('should allow an empty nullable string with options', async () => {
+    const schema = yd.string().options({
+      nullable: true,
+      allowEmpty: true,
+    });
+    await assertPass(schema, '');
     await assertPass(schema, null);
   });
 });
